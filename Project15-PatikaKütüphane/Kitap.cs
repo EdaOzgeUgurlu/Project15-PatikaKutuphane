0b1affbfd485839ff65fa9107ae3f92d28a958d5
Eda Özge UĞURLU <edaozgeugurlu@gmail.com>
2024-10-27 18:12:28 +0300

    Proje dosyası ekle.

---
+++ Project15-PatikaKütüphane/Kitap.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project15_PatikaKütüphane
+{
+    internal class Kitap
+    {
+        // Gerekli alanları tanımlıyoruz.
+        public string kitapAd;      // Kitap adı
+        public string yazarAd;      // Yazar adı
+        public string yazarSoyadi;  // Yazar soyadı
+        public int sayfaSayisi;     // Sayfa sayısı
+        public string yayinevi;     // Yayın evi
+        public DateTime tarih;      // Kayıt tarihi
+
+        // Gerekli alanların özelliklerini tanımlıyoruz.
+        public string KitabAd // Kitap adını almak ve ayarlamak için özellik
+        {
+            get // Kitap adı değeri alınırken kullanılacak olan get
+            {
+                return kitapAd; // Değişkenin değerini döndür
+            }
+            set // Kitap adı değeri atanırken kullanılacak olan set
+            {
+                kitapAd = value; // Atanan değeri değişkene kaydet
+            }
+        }
+
+        public string YazarAd 
+        {
+            get
+            {
+                return yazarAd; 
+            }
+            set
+            {
+                yazarAd = value; 
+            }
+        }
+
+        public string YazarSoyadi 
+        {
+            get
+            {
+                return yazarSoyadi; 
+            }
+            set
+            {
+                yazarSoyadi = value; 
+            }
+        }
+
+        public int SayfaSayisi 
+        {
+            get
+            {
+                return sayfaSayisi; 
+            }
+            set
+            {
+                sayfaSayisi = value;
+            }
+        }
+
+        public string Yayinevi 
+        {
+            get
+            {
+                return yayinevi; 
+            }
+            set
+            {
+                yayinevi = value; 
+            }
+        }
+
+        public DateTime Tarih 
+        {
+            get
+            {
+                return tarih; 
+            }
+            set
+            {
+                tarih = value; 
+            }
+        }
+
+        // Parametreli constructor
+        public Kitap(string kitapAd, string yazarAd, string yazarSoyadi, int sayfaSayisi, string yayinevi)
+        {
+            // Parametrelerden gelen değerleri alanlara atıyoruz
+            this.kitapAd = kitapAd;
+            this.yazarAd = yazarAd;
+            this.yazarSoyadi = yazarSoyadi;
+            this.sayfaSayisi = sayfaSayisi;
+            this.yayinevi = yayinevi;
+            this.tarih = DateTime.Now; // Kayıt tarihi olarak şu anki zamanı alıyoruz
+        }
+
+        // Varsayılan constructor
+        public Kitap()
+        {
+            // Varsayılan oluşturucuda kayıt tarihi olarak şu anki zamanı alıyoruz
+            Tarih = DateTime.Now;
+        }
+
+        // Kitap bilgilerini yazdıran metot
+        public void KitapBilgisi()
+        {
+            // Kitap bilgilerini konsola yazdırıyoruz
+            Console.WriteLine($"Kitap Adı: {kitapAd} \nYazar Adı-Soyadı: {yazarAd} {yazarSoyadi} \nSayfa Sayısı: {sayfaSayisi} \nYayın Evi: {yayinevi} ");
+            Console.WriteLine($"Kayıt tarihi:  {tarih}");
+        }
+    }
+}
---
+++ Project15-PatikaKütüphane/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using Project15_PatikaKütüphane; // Projedeki Kitap sınıfını içe aktarıyoruz
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Kitap nesnelerini oluşturuyoruz
+        Kitap kitap1 = new Kitap(); // İlk kitap nesnesi
+        Kitap kitap2 = new Kitap(); // İkinci kitap nesnesi
+        Kitap kitap3 = new Kitap(); // Üçüncü kitap nesnesi
+
+        // İlk kitabın bilgilerini ayarlıyoruz
+        kitap1.kitapAd = "Adı Aylin";
+        kitap1.yazarAd = "Ayşe";
+        kitap1.yazarSoyadi = "Kulin";
+        kitap1.sayfaSayisi = 398;
+        kitap1.yayinevi = "Remzi Kitapevi";
+
+        // İkinci kitabın bilgilerini ayarlıyoruz
+        kitap2.kitapAd = "Ana";
+        kitap2.yazarAd = "Maksim";
+        kitap2.yazarSoyadi = "Gorki";
+        kitap2.sayfaSayisi = 400;
+        kitap2.yayinevi = "Can Kitabevi";
+
+        // Üçüncü kitabın bilgilerini ayarlıyoruz
+        kitap3.kitapAd = "Düşüş";
+        kitap3.yazarAd = "Albert";
+        kitap3.yazarSoyadi = "Camus";
+        kitap3.sayfaSayisi = 350;
+        kitap3.yayinevi = "Can Kitapevi";
+
+        // Kitap bilgilerini konsola yazdırıyoruz
+        kitap1.KitapBilgisi(); // İlk kitabın bilgilerini yazdır
+        Console.WriteLine();
+        kitap2.KitapBilgisi(); // İkinci kitabın bilgilerini yazdır
+        Console.WriteLine();
+        kitap3.KitapBilgisi(); // Üçüncü kitabın bilgilerini yazdır
+        Console.WriteLine();
+    }
+}
+
+// Class : Nesnenin yapısını ve işlevlerini tanımlayan bir şablondur. Kitap adında bir sınıf oluşturduk. Bu sınıf, kitap nesnelerini temsil eder ve kitapla ilgili özellikler (property) ile davranışları (metotlar) içerir.
+//Özellikler (Property): sınıf içindeki verileri temsil eder. Kitap sınıfında, kitabın adı, yazarı, sayfa sayısı, yayınevi ve kayıt tarihi gibi bilgileri saklayan özellikler yer alır. Bu özellikler public durumda, dışarıdan erişime ve değişime açıktır.
+//Yeni Nesne Oluşturma new anahtar kelimesi: sınıf içerisinde yeni bir nesne oluşturmak için kullanılır. Bu sayede sınıfın bir örneği (instance) yaratılır. Örneğin, kitap1, kitap2 ve kitap3 adında ayrı ayrı kitap nesnesi oluşturduk.
+//Yapıcı Metot(Constructor) : yeni bir nesne oluşturulduğunda ilk olarak çalışan özel bir metottur. Bu metot, nesnenin başlangıç durumunu ayarlamak için kullanılır. Bu çalışmada biri parametre almayan (varsayılan yapıcı), diğeri ise parametre alarak bilgileri nesne oluşturulurken ilgili özelliklere atayan yapıcı olmak üzere iki yapıcı metot oluşturduk.
+
+
